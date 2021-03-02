dbb07e9b86ce06ab58bf40bbe367769edace9646
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2021-03-02 12:37:12 +0300

    Adding test support for types and objects creation

--- kotlin-native/runtime/src/legacymm/cpp/Memory.cpp
+++ kotlin-native/runtime/src/legacymm/cpp/Memory.cpp
@@ -3296,6 +3296,10 @@ void RestoreMemory(MemoryState* memoryState) {
     ::memoryState = memoryState;
 }
 
+void ClearMemoryForTests(MemoryState*) {
+    // Nothing to do, DeinitMemory will do the job.
+}
+
 OBJ_GETTER(AllocInstanceStrict, const TypeInfo* type_info) {
   RETURN_RESULT_OF(allocInstance<true>, type_info);
 }
